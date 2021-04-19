13ce14ad988dd5558670df7dee95ec9f87c83b96
cyberwr3nch <dhaneshsivasamy07@gmail.com>
2021-04-19 21:20:26 +0530

    narnia0 writeup added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210419.0022';
+const cacheName = 'chirpy-20210419.2350';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
